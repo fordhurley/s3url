6e7b760027592a2ec1cbacd78ded0be9e56213ae
Ford Hurley <ford.hurley@gmail.com>
2014-04-06 11:20:05 -0400

    Read in markdown README and convert to rst for PyPI

--- setup.py
+++ setup.py
@@ -3,6 +3,29 @@ from setuptools import setup
 
 from s3url import __version__
 
+
+# From https://gist.github.com/aubricus/9184003
+long_description = ''
+try:
+    import subprocess
+    import pandoc
+
+    process = subprocess.Popen(
+        ['which pandoc'],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+    pandoc_path = process.communicate()[0]
+    pandoc_path = pandoc_path.strip('\n')
+    pandoc.core.PANDOC_PATH = pandoc_path
+    doc = pandoc.Document()
+    doc.markdown = open('README.md').read()
+    long_description = doc.rst
+except:
+    long_description = open('README.md').read()
+
+
 setup(
     name='s3url',
     version=__version__,
@@ -11,6 +34,7 @@ setup(
     url='https://github.com/fordhurley/s3url',
     description='A simple command line utility for generating time-sensitive, '
                 'sharable urls for private s3 files',
+    long_description=long_description,
     license='WTFPL v2',
 
     scripts=['scripts/s3url'],
