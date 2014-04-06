56126c106748915b78eb3f111309a58725b4e144
Ford Hurley <ford.hurley@gmail.com>
2014-04-05 21:56:40 -0400

    Fix up setup.py and add setup.cfg

--- setup.py
+++ setup.py
@@ -7,14 +7,18 @@ setup(
     name='s3url',
     version=__version__,
     author='Ford Hurley',
+    author_email='ford.hurley@gmail.com',
+    url='https://github.com/fordhurley/s3url',
     description='A simple command line utility for generating time-sensitive, '
-                'sharable urls for private s3 files'
+                'sharable urls for private s3 files',
     long_description=open('README.md').read(),
-    license='WTFPL v2'
+    license='WTFPL v2',
 
     scripts=['scripts/s3url'],
+    packages=['s3url'],
     install_requires=[
-        'boto>=2.17'
+        'boto>=2.17',
         'docopt>=0.6.1'
-    ]
+    ],
+    tests_require=['nose>=1.0']
 )
